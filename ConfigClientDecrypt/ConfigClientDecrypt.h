774da638dc2fa23343697f06d4746a451cb38abc
Stephen Anthony <steve@darjeelingapps.com>
2022-08-16 10:32:55 +0100

    Initial Commit

---
+++ ConfigClientDecrypt/ConfigClientDecrypt.h
@@ -0,0 +1,18 @@
+//
+//  ConfigClientDecrypt.h
+//  ConfigClientDecrypt
+//
+//  Created by Stephen Anthony on 16/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConfigClientDecrypt.
+FOUNDATION_EXPORT double ConfigClientDecryptVersionNumber;
+
+//! Project version string for ConfigClientDecrypt.
+FOUNDATION_EXPORT const unsigned char ConfigClientDecryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfigClientDecrypt/PublicHeader.h>
+
+
